d1ce25f1281b667c2ce58a561f6f8f3b77af4c8d
Thenis <knnz_nn@yahoo.com>
2016-08-11 12:24:55 +0300

    Added Average Points and Total Points to Users

--- Forum/Models/Category.cs
+++ Forum/Models/Category.cs
@@ -1,32 +1,32 @@
 namespace Forum.Models
 {
-    using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.Data;
+	using System.Collections.Generic;
+	using System.ComponentModel.DataAnnotations;
+	using System.Data;
 
-    public partial class Category
-    {
-        public Category()
-        {
-            Threads = new List<ForumThread>();
-        }
+	public partial class Category
+	{
+		public Category()
+		{
+			Threads = new List<ForumThread>();
+		}
 
 		[Key]
-        public int Id
-        {
-            get; set;
-        }
+		public int Id
+		{
+			get; set;
+		}
 
-        [Required]
-        [StringLength(100)]
-        public string Name
-        {
-            get; set;
-        }
+		[Required]
+		[StringLength(100)]
+		public string Name
+		{
+			get; set;
+		}
 
-        public List<ForumThread> Threads
-        {
-            get; set;
-        }
-    }
+		public List<ForumThread> Threads
+		{
+			get; set;
+		}
+	}
 }
