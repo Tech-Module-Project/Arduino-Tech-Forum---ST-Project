ba2f6c08734e9e10d9e76d278457eb36756e1a48
Thenis <knnz_nn@yahoo.com>
2016-08-15 17:00:03 +0300

    Added a live search for users in the admin panel https://localhost:44393/Admin/Users

---
+++ Forum/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Forum.Models;
+
+namespace Forum.Controllers
+{
+    public class AdminController : Controller
+    {
+        protected readonly ApplicationDbContext db = new ApplicationDbContext();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Users()
+        {
+            var users = db.Users.ToList();
+
+            return View(users);
+        }
+
+        public PartialViewResult Search(string query)
+        {
+            var result = db.Users.Where(u => u.UserName.ToLower().Contains(query.ToLower())).ToList();
+
+            return PartialView("_UsersResult", result);
+        }
+    }
+}
